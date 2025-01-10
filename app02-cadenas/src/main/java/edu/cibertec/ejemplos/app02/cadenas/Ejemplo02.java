e614e0bd4be531fca7d95b9604523bd54c30f14a
JorgePorrasJ <jorgeporrasj22@gmail.com>
2025-01-09 21:12:46 -0500

    ejemplo 2 continuar

---
+++ app02-cadenas/src/main/java/edu/cibertec/ejemplos/app02/cadenas/Ejemplo02.java
@@ -0,0 +1,23 @@
+package edu.cibertec.ejemplos.app02.cadenas;
+
+/**
+ *
+ * @author Jorge
+ */
+public class Ejemplo02 {
+    
+    public static void main(String[] args) {
+        // Definir algunas variables
+        String nombre = "Jorge";
+        String apellido = "Porras";
+        String dni = "73977230"; // Para dni, teléfono o # de tc, usamos String
+        // Vamos a construir el nombre un usuario bajo el siguiente esquema:
+        // primera letra del nombre + el apellido + dos últimos dígitos del DNI
+        // Todo debe estar en minúscula, Ej: jporras30
+        
+        //continuar
+        
+        
+        
+    }
+}
