83ffe1971872015e636527e326001dc3b0a755be
JorgePorrasJ <jorgeporrasj22@gmail.com>
2025-01-09 20:25:56 -0500

    tarea 20

---
+++ ejercicios-basicos/src/main/java/edu/cibertec/ejemplos/ejercicios/basicos/Ejercicio20.java
@@ -0,0 +1,12 @@
+package edu.cibertec.ejemplos.ejercicios.basicos;
+
+/**
+ *
+ * @author Jorge
+ */
+public class Ejercicio20 {
+    
+    public static void main(String[] args) {
+        // tarea 20
+    }
+}
