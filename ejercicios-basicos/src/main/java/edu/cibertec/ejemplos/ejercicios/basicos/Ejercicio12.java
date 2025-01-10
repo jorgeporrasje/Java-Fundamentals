5c9738a8f741b906092c1e0eb7cb3cc87ce8cc3b
JorgePorrasJ <jorgeporrasj22@gmail.com>
2025-01-09 20:24:45 -0500

    tarea 12

---
+++ ejercicios-basicos/src/main/java/edu/cibertec/ejemplos/ejercicios/basicos/Ejercicio12.java
@@ -0,0 +1,12 @@
+package edu.cibertec.ejemplos.ejercicios.basicos;
+
+/**
+ *
+ * @author Jorge
+ */
+public class Ejercicio12 {
+    
+    public static void main(String[] args) {
+        // tarea 12
+    }
+}
