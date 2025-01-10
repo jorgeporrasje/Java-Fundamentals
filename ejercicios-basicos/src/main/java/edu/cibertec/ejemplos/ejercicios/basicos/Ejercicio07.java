d33dc25a674975a5c62a08733160a0f2e525b36b
JorgePorrasJ <jorgeporrasj22@gmail.com>
2025-01-09 20:23:41 -0500

    tarea 07

---
+++ ejercicios-basicos/src/main/java/edu/cibertec/ejemplos/ejercicios/basicos/Ejercicio07.java
@@ -0,0 +1,12 @@
+package edu.cibertec.ejemplos.ejercicios.basicos;
+
+/**
+ *
+ * @author Jorge
+ */
+public class Ejercicio07 {
+    
+    public static void main(String[] args) {
+        //tarea
+    }
+}
